45afc24bf01e8e91c6372c710720d5d057053648
Hitesh Trivedi <htr@openerp.com>
2014-12-18 14:47:49 +0530

    [IMP] web : Add Indicator to notify user when connection get lost

--- addons/web/static/src/js/chrome.js
+++ addons/web/static/src/js/chrome.js
@@ -252,6 +252,21 @@ instance.web.CrashManager = instance.web.Class.extend({
         if (!this.active) {
             return;
         }
+        if (error.code == -32098) {
+            $.blockUI({ message: '' , overlayCSS: {'z-index': 9999, backgroundColor: '#FFFFFF', opacity: 0.0, cursor: 'wait'}});
+            $('<div class="oe_indicator">' + _t("Trying to connect...") + '<i class="fa fa-refresh fa-spin"></i></div>').prependTo("body");
+            var timeinterval = setInterval(function(){
+                openerp.jsonRpc('/web/webclient/version_info').then(function() {
+                    clearInterval(timeinterval);
+                    $('.oe_indicator').html(_t("You are back online"));
+                    $('.oe_indicator').delay(2000).fadeOut('slow',function(){
+                        $('.oe_indicator').remove();
+                    });
+                    $.unblockUI();
+                });
+            }, 2000);
+            return;
+        }
         var handler = instance.web.crash_manager_registry.get_object(error.data.name, true);
         if (handler) {
             new (handler)(this, error).display();
