1d098e46e49f220ea856574e570f316216ae247f
Jigar Patel <jat@openerp.com>
2015-01-15 11:39:59 +0530

    [REF] webssite_twitter_wall: Refactore website_twitter_wall.js code

--- addons/website_twitter_wall/static/src/js/website_twitter_wall.js
+++ addons/website_twitter_wall/static/src/js/website_twitter_wall.js
@@ -12,26 +12,24 @@ openerp.website.tweet_wall = openerp.Class.extend({
     template: 'twitter_tweets',
     init: function($el, wall_id, interval_time) {
         this.$el = $el;
-        this.get_data_duration = interval_time || 5500;
-        this.show_tweet_duation = interval_time || 5000;
+        this.interval_time = interval_time || 5000;
         this.wall_id = wall_id;
         this.show_tweet = [];
-        this.shown_tweet = [];
         this.last_tweet_id = 0;
     },
 
+    // Hide header and footer, Start to get and display tweet based on interval time
     start: function() {
         var self = this;
-        if(window.location.href.split("/")[3] == "twitter_wall") {
-            $('#oe_main_menu_navbar, header, footer').css("display", "none");
-        }
-        setInterval(function() { return self.get_data(); }, this.get_data_duration);
-        setInterval(function() { self.process_tweet(); }, this.show_tweet_duation);
+        $('#oe_main_menu_navbar, header, footer').css("display", "none");
+        setInterval(function() { return self.get_data(); }, this.interval_time);
+        setInterval(function() { self.process_tweet(); }, this.interval_time);
     },
 
+    // Get tweet
     get_data: function() {
         var self = this;
-        return openerp.jsonRpc("/twitter_wall/pull_tweet/" + self.wall_id, 'call', {'last_tweet': self.last_tweet_id}).done(function(data) {
+        return openerp.jsonRpc("/twitter_wall/pull_tweet/" + this.wall_id, 'call', {'last_tweet': this.last_tweet_id}).done(function(data) {
             if (data){
                 self.last_tweet_id = data.id;
                 self.show_tweet = self.show_tweet.concat(data);
@@ -39,18 +37,9 @@ openerp.website.tweet_wall = openerp.Class.extend({
         });
     },
 
+    // Display tweet
     process_tweet: function() {
-        if (this.show_tweet.length) {
-            var tweet = this.show_tweet.shift();
-            this.shown_tweet.push(tweet);
-            this.animate_tweet(openerp.qweb.render("twitter_tweets", {'res': tweet}));
-        }
+        if (this.show_tweet.length)
+            $(openerp.qweb.render("twitter_tweets", {'res': this.show_tweet.shift()})).prependTo(this.$el);
     },
-
-    animate_tweet: function(tweet_html) {
-        $(tweet_html).prependTo(this.$el);
-        setTimeout(function() {
-            $('.live-tweet').slideDown("slow");
-        }, 1600);
-    }
 });
