2bdfa6397fb930a4cd10d6569857874490601ea9
Damien Bouvy <dbo@odoo.com>
2015-02-06 09:09:22 +0100

    invoice line: new fields + views recurring invoices visible in contract template

--- addons/website_contract/models/account.py
+++ addons/website_contract/models/account.py
@@ -1,19 +1,54 @@
 # -*- coding: utf-8 -*-
 
-from openerp import fields, models
+from openerp import api, fields, models
+
 
 class account_analytic_account(models.Model):
     _name = 'account.analytic.account'
     _inherit = 'account.analytic.account'
+
+    @api.one
+    @api.depends('recurring_invoice_line_ids')
+    def _compute_base_price(self):
+        base_price = 0
+        for invoice_line in self.recurring_invoice_line_ids:
+            if invoice_line.type == 'mandatory':
+                base_price += invoice_line.price_subtotal
+        self.base_price = base_price
+
+    def _search_upper(self, operator, value):
+        return [('name', operator, value)]
 
     plan_description = fields.Html(
         string='Plan Description',
         help="Describe this contract in a few lines",
         )
     selectable_by_customer = fields.Boolean(
-        string='User selectable', 
-        help="""Leave this unchecked if you don't want this contract 
-        template to be availableto the customer in the frontend 
-        (for a free trial, for example)""", 
+        string='User selectable',
+        help="""Leave this unchecked if you don't want this contract
+        template to be availableto the customer in the frontend
+        (for a free trial, for example)""",
         default=True
+        )
+    base_price = fields.Float(compute=_compute_base_price, store=True, search=_search_upper)
+
+
+class account_analytic_invoice_line(models.Model):
+    _name = "account.analytic.invoice.line"
+    _inherit = "account.analytic.invoice.line"
+
+    type = fields.Selection(
+        selection=[('mandatory', 'Mandatory'), ('optional', 'Optional')],
+        help="A mandatory invoice line cannot be removed from the contract",
+        default="mandatory"
+        )
+    user_addable = fields.Boolean(
+        string="User can add line",
+        help="If checked, the user is able to add this line to his contract himself",
+        default=False
+        )
+    user_removable = fields.Boolean(
+        string="User can remove line",
+        help="If checked, the user is able to remove this line from his contract himself",
+        default=False
         )
