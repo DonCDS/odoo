cd598d2e9b0deefda0497cc416f51682f51d506b
ptr <ptr@openerp.com>
2015-02-10 11:06:10 +0530

    [IMP] hr_recruitment: remove files and add code in related fles.

--- addons/hr_recruitment/hr_recruitment.py
+++ addons/hr_recruitment/hr_recruitment.py
@@ -234,6 +234,22 @@ class hr_applicant(osv.Model):
             res[app_id] = self.pool['ir.attachment'].search_count(cr, uid, [('res_model', '=', 'hr.applicant'), ('res_id', '=', app_id)], context=context)
         return res
 
+    def _get_index_content(self, cr, uid, ids, fields, args, context=None):
+        res = dict.fromkeys(ids, '')
+        Attachment = self.pool.get('ir.attachment')
+        attachment_ids = Attachment.search(cr, uid, [('res_model', '=', 'hr.applicant'), ('res_id', 'in', ids)], context=context)
+        for attachment in Attachment.browse(cr, uid, attachment_ids, context=context):
+            res[attachment.res_id] += attachment.index_content or ''
+        return res
+
+    def _content_search(self, cr, user, obj, name, args, context=None):
+        record_ids = set()
+        Attachment = self.pool.get('ir.attachment')
+        args = ['&'] + args + [('res_model', '=', 'hr.applicant')]
+        att_ids = Attachment.search(cr, user, args, context=context)
+        record_ids = set(att.res_id for att in Attachment.browse(cr, user, att_ids, context=context))
+        return [('id', 'in', list(record_ids))]
+
     _columns = {
         'name': fields.char('Subject / Application Name', required=True),
         'active': fields.boolean('Active', help="If the active field is set to false, it will allow you to hide the case without removing it."),
@@ -282,6 +298,7 @@ class hr_applicant(osv.Model):
         'user_email': fields.related('user_id', 'email', type='char', string='User Email', readonly=True),
         'attachment_number': fields.function(_get_attachment_number, string='Number of Attachments', type="integer"),
         'employee_name': fields.related('emp_id', 'name', type='char', string='Employee Name'),
+        'index_content': fields.function(_get_index_content, fnct_search=_content_search, string='Index Content', type="text"),
     }
 
     _defaults = {
