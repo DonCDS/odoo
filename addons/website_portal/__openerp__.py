722448b9064e9f537c4db5d17962bd62b57062a8
Damien Bouvy <dbo@odoo.com>
2015-02-10 17:16:05 +0100

    [ADD] website_portal - manage your sale documents from the frontend in the 'My Account' page - move 'My Orders' page from website_sale to website_portal to manage your orders and quotations - update website_sale_digital and website_sale_stock controllers and view

---
+++ addons/website_portal/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source ERP and CRM
+#    Copyright (C) 2015-Today Odoo SA (<http://www.odoo.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Website Portal',
+    'category': 'Website',
+    'summary': 'Account Management Frontend for your Customers',
+    'version': '1.0',
+    'description': """
+Allows your customers to manage their account from a beautiful web interface.
+        """,
+    'author': 'Odoo SA',
+    'website': 'https://www.odoo.com/',
+    'depends': [
+        'sale',
+        'website',
+    ],
+    'data': [
+        'views/templates.xml',
+        'security/ir.model.access.csv'
+    ],
+    'qweb': [
+    ],
+    'demo': [
+        'demo.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ addons/website_portal/controllers/main.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+import werkzeug
+
+from openerp import http
+from openerp.http import request
+from openerp.tools.translate import _
+
+
+class website_account(http.Controller):
+    @http.route(['/account'], type='http', auth="public", website=True)
+    def account(self):
+        partner = request.env.user.partner_id
+        values = {}
+
+        res_sale_order = request.env['sale.order']
+        res_invoices = request.env['account.invoice']
+        cust_quotations = res_sale_order.search([
+            '&',
+            '|',
+            ('partner_id.id', '=', partner.id),
+            ('partner_id.id', '=', partner.commercial_partner_id.id),
+            ('state', '=', 'sent')
+            ])
+        cust_orders = res_sale_order.search([
+            '&',
+            '|',
+            ('partner_id.id', '=', partner.id),
+            ('partner_id.id', '=', partner.commercial_partner_id.id),
+            ('state', 'in', ['progress', 'manual', 'shipping_except', 'invoice_except', 'done'])
+            ])
+        cust_invoices = res_invoices.search([
+            '&',
+            '|',
+            ('partner_id.id', '=', partner.id),
+            ('partner_id.id', '=', partner.commercial_partner_id.id),
+            ('state', 'in', ['open', 'paid', 'cancelled'])
+            ])
+
+        values['cust_quotations'] = cust_quotations
+        values['cust_orders'] = cust_orders
+        values['cust_invoices'] = cust_invoices
+
+        # get customer sales rep
+        if partner.user_id:
+            sales_rep = partner.user_id
+        elif partner.commercial_partner_id and partner.commercial_partner_id.user_id:
+            sales_rep = partner.commercial_partner_id.user_id
+        else:
+            sales_rep = False
+        values['sales_rep'] = sales_rep
+        values['company'] = request.website.company_id
+
+        return request.website.render("website_portal.account", values)
+
+    @http.route([
+                '/account/orders',
+                '/account/orders/page/<int:page>',
+                '/account/orders/<int:order>'
+                ], type='http', auth="user", website=True)
+    def orders_followup(self, page=1, order=None, by=5, **post):
+        partner = request.env['res.users'].browse(request.uid).partner_id
+        domain = [
+            ('partner_id', '=', partner.id),
+            ('state', 'not in', ['draft', 'cancel'])
+            ]
+        if order:
+            domain.append(('id', '=', order))
+        orders = request.env['sale.order'].sudo().search(domain)
+
+        nbr_pages = max((len(orders) / by) + (1 if len(orders) % by > 0 else 0), 1)
+        page = min(page, nbr_pages)
+        pager = request.website.pager(
+            url='/account/orders', total=nbr_pages, page=page, step=1,
+            scope=by, url_args=post
+        )
+        orders = orders[by*(page-1):by*(page-1)+by]
+
+        order_invoice_lines = {}
+        for o in orders:
+            invoiced_lines = request.env['account.invoice.line'].sudo().search([('invoice_id', 'in', o.invoice_ids.ids)])
+            order_invoice_lines[o.id] = {il.product_id.id: il.invoice_id for il in invoiced_lines}
+
+        return request.website.render("website_portal.orders_followup", {
+            'orders': orders,
+            'order_invoice_lines': order_invoice_lines,
+            'pager': pager,
+        })
